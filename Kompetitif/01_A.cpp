83b61f49dd6ba46a19abb5f9a6f59a6fc4c7b118
arv <marvin07ar@gmail.com>
2023-07-22 11:11:14 +0700

    22/7/2023

--- Kompetitif/01_A.cpp
+++ Kompetitif/01_A.cpp
@@ -1,53 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+#define int      long long
+#define all(a)   (a).begin(), (a).end()
+#define pb       push_back
+#define pii      pair<int, int>
+#define ff       first
+#define ss       second
+ 
+#define lcm(a,b) (a * (b / __gcd(a,b)))
+#define dbg(x)   cout << '(' << ' ' << #x << ' ' << '=' << ' ' << x << ' ' << ')' << endl;
+ 
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+ 
+// UB = >, LB = >=
 
-#define ar array
-#define v vector
-#define ll long long
-#define ld long double 
-#define sza(x) ((int)x.size()) // RETURNS ARRAY SIZE
-#define all(a) (a).begin(), (a).end() // FOR VECTOR
-#define allA(a, batas) (a), (a)+batas  // FOR ARRAY
-
-const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
-const ld EPS = 1e-9;
-
-struct PESERTA {
-    string ID;
+struct p {
+    string id;
     int score[3];
 };
 
-bool rule(PESERTA P1, PESERTA P2) {
-    if (P1.score[2] == P2.score[2]) return P1.score[2] < P2.score[2];
-    if (P1.score[1] == P2.score[1]) return P1.score[1] < P2.score[1];
-    return P1.score[0] < P2.score[0];
-    
+bool rule(p x, p y) {
+    if(x.score[2] == y.score[2]) return x.score[2] < y.score[2];
+    if(x.score[1] == y.score[1]) return x.score[1] < y.score[1];
+    return x.score[0] < y.score[0];
 }
-
-void solve() {
-    PESERTA psrt[81];
-    int N, M; bool flag = false; string ID; cin >> N >> M >> ID;;
-    for (int i = 0; i < N; i++){
-        cin >> psrt[i].ID >> psrt[i].score[0] >> psrt[i].score[1] >> psrt[i].score[2];
+ 
+void solve(){
+    int n, m; string id; cin >> n >> m >> id;
+    vector<p> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i].id >> v[i].score[0] >> v[i].score[1] >> v[i].score[2];
     }
-
-    sort(allA(psrt, N), rule);
-
-    for (int i = 0; i < M; i++) {
-        if (psrt[i].ID == ID) {flag = true; break;}
+    sort(all(v), rule);
+    for(int i = 0; i < m; i++) {
+        if(v[i].id == id) cout << "YA" << endl;
     }
-
-    (flag ? cout << "YA" << endl : cout << "TIDAK" << endl);
+    cout << "TIDAK" << endl;
 }
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int tc; cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
-        solve();
-    }
+ 
+signed main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int tc; cin >> tc; while(tc--) solve();
+    return 0;
 }
--- Kompetitif/01_C.cpp
+++ Kompetitif/01_C.cpp
@@ -1,33 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void solve(string front, string back) {
-    string input; cin >> input; bool con = false;
-    if (input.length() >= back.length()+front.length()){
-        if (front.length() != 0 || back.length() != 0) {
-            if (front == input.substr(0, front.length()) && back == input.substr(input.length()-back.length(), input.length())) cout << input << endl;
+ 
+#define int      long long
+#define all(a)   (a).begin(), (a).end()
+#define pb       push_back
+#define pii      pair<int, int>
+#define ff       first
+#define ss       second
+ 
+#define lcm(a,b) (a * (b / __gcd(a,b)))
+#define dbg(x)   cout << '(' << ' ' << #x << ' ' << '=' << ' ' << x << ' ' << ')' << endl;
+ 
+const int MOD  = 1e9 + 7;
+const int INF  = 1e9;
+ 
+// UB = >, LB = >=
+ 
+void solve(){
+    string s, front = "", back = ""; cin >> s;
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == '*') {
+            for(int j = i+1; j < s.length(); j++) {
+                back += s[j];
+            }
+            break;
         }
-        else {
-            cout << input << endl;
+        front += s[i];
+    }
+    string a; int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if (a.length() >= front.length() + back.length()) {
+            if(front.length() + back.length() == 0) {
+                cout << a << endl;
+            } else {
+                if (front == a.substr(0, front.length()) && back == a.substr(a.length()-back.length(), a.length())) cout << a << endl;
+            }
         }
     }
 }
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    string wildcard, front = "", back = ""; int index; cin >> wildcard;
-    for (int i = 0; i < wildcard.length(); i++){
-        if (wildcard[i] == '*') index = i;
-    } for (int i = 0; i < index; i++){
-        front += wildcard[i];
-    } for (int i = index+1; i < wildcard.length(); i++){
-        back += wildcard[i];
-    }
-
-    int tc; cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        solve(front, back);
-    }
+ 
+signed main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
 }
