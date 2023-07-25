91efdf6fa862bbb6cb2894461cfa48041fad3791
arv <marvin07ar@gmail.com>
2023-07-25 21:25:40 +0700

    25/7/2023

--- Kompetitif/03_B.cpp
+++ Kompetitif/03_B.cpp
@@ -1,32 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// #define all(a) (a).begin(), (a).end() // FOR VECTOR
-
-// void solve() {
-//     int N, M, K, x, y;; cin >> N >> M >> K; bool con = false;
-//     int papan[105][105], val[105][105];
-//     for (int i = 0; i < 105; i++) for(int j = 0; j < 105; j++) {papan[i][j] = 1; val[i][j] = 1;}
-//     for (int i = 1; i <= N; i++) for(int j = 1; j <= M; j++) cin >> papan[i][j];
-
-//     for (int i = 1; i <= M; i++) {
-//         for(int j = 1; j <= N; j++){
-//             val[j][i] *= papan[j][i-1] * papan[j][i+1] * papan[j+1][i] * papan[j-1][i];
-//             if (val[j][i] == K && !con) {
-//                 x = j; y = i; con = true;
-//             }
-//         }
-//     }
-//     (con ? cout << x << " " << y << endl : cout << "0 0" << endl);
-// }
-
-// int main() {
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(0); cout.tie(0);
-    
-//     solve();
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -46,8 +17,8 @@ const int INF  = 1e9;
 // UB = >, LB = >=
  
 void solve(){
-    int n, m, k, x, y, grid[105][105]; cin >> n >> m >> k;
-    memset(grid, 1, sizeof grid);
+    int n, m, k, x, y, grid[105][105], val[105][105]; cin >> n >> m >> k;
+    for(int i = 0; i < 105; i++) for(int j = 0; j < 105; j++) grid[i][j] = 1, val[i][j] = 1;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= m; j++) {
             cin >> grid[i][j];
@@ -56,10 +27,8 @@ void solve(){
 
     for(int i = 1; i <= m; i++) {
         for(int j = 1; j <= n; j++) {
-            if(grid[j][i-1] * grid[j][i+1] * grid[j-1][i] * grid[j+1][i] == k) {
-                cout << i << " " << j << endl;
-                return;
-            }
+            val[j][i] *= grid[j][i-1] * grid[j][i+1] * grid[j+1][i] * grid[j-1][i];
+            if(val[j][i] == k) {cout << j << " " << i << endl;return;}
         }
     }
     cout << "0 0" << endl;
